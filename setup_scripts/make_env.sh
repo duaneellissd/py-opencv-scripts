7c8dc8cbce084d85511e7ed0bf87297284423e84
Duane Ellis <duane@duaneellis.com>
2018-07-05 12:06:26 -0700

    Let name be specified

--- setup_scripts/make_env.sh
+++ setup_scripts/make_env.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [ x"${ENV_NAME}" == x"" ]
+if [ x${1} == x ]
 then
-    ENV_NAME=cv
+    echo "Usage: ${0} ENVNAME"
+    exit 1
 fi
 
-# blow away old virtual env
-rm -rf ${HOME}/.virtualenvs/${ENV_NAME}
+ENV_NAME=${1}
 
-# I only use Python3 
+# blow away old virtual env
+echo "Remove old ENV: ${ENV_NAME}"
+set -x
+rm -rf "${HOME}/.virtualenvs/${ENV_NAME}"
+set +x
+
+# I only use Python3
+export WORKON_HOME=${HOME}/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
 mkvirtualenv ${ENV_NAME} --system-site-packages -p python3
 
