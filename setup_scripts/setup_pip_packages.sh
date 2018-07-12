eb71343b88635e4a1a219f1c9210426a707587e1
Duane Ellis <duane@duaneellis.com>
2018-07-12 11:27:44 -0700

    more packages

--- setup_scripts/setup_pip_packages.sh
+++ setup_scripts/setup_pip_packages.sh
@@ -14,4 +14,7 @@ do
     sudo ${PIP} install scipy
     sudo ${PIP} install markdown
     sudo ${PIP} install h5py
+    sudo ${PIP} install scikit-image
+    sudo ${PIP} install dlib
+    sudo ${PIP} JSON_minify
 done
